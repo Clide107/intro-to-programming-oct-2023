83e7e589740cbdfb9c0f2b027ebcf556f362eaf7
Jess Atkinson <jesslynn1007@gmail.com>
2023-10-24 16:54:14 -0400

    day 2 end

---
+++ stringcalculator-tuesday/StringCalculator.cs
@@ -0,0 +1,74 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (numbers.Contains(",") && numbers.Contains("\n"))
+        {
+            //List<int> list = new List<int>();
+            int total = 0;
+            List<string> myNums = numbers.Split(',').ToList();
+            //for (int i = 0; i < myNums.Count; i++)
+            //{
+            //string number = myNums[i];
+            //if (number.Contains("\n")) 
+            //{
+            //    int numberNum = number.
+            //    list.Add(number.Split(","));
+            //}
+            //}
+
+            string nNumbers = "";
+
+            for (int i = 0; i < myNums.Count; i++)
+            {
+                nNumbers += myNums[i];
+            }
+
+            List<int> list = nNumbers.Split('\n').Select(int.Parse).ToList();
+
+            for (int i = 0; i < myNums.Count; i++)
+            {
+                total += list[i];
+            }
+            return total;
+        }
+
+        if (numbers.Contains(","))
+        {
+            int total = 0;
+            List<int> myNums = numbers.Split(',').Select(int.Parse).ToList();
+            for (int i = 0; i < myNums.Count; i++)
+            {
+                total += myNums[i];
+            }
+            return total;
+        }
+
+        if (numbers.Contains("\n"))
+        {
+            int total = 0;
+            List<int> myNums = numbers.Split('\n').Select(int.Parse).ToList();
+            for (int i = 0; i < myNums.Count; i++)
+            {
+                total += myNums[i];
+            }
+            return total;
+        }
+
+
+
+        if (numbers == "")
+        {
+            return 0;
+
+        }
+        else
+        {
+            return int.Parse(numbers);
+        }
+    }
+}
---
+++ stringcalculator-tuesday/StringCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("118", 118)]
+    public void SingleDigit(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData("1,1", 2)]
+    [InlineData("2,2", 4)]
+    public void DoubleDigit(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,1,3,5", 10)]
+    [InlineData("2,6,7", 15)]
+    public void UnknownNumbers(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n1", 2)]
+    [InlineData("2\n2", 4)]
+    [InlineData("2\n2,3", 7)]
+    public void NewLines(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+}
